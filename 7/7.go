7551748f1094617b87763460aeed2e0a3bc0c554
Nathan Hoersch <nathan.hoersch@gmail.com>
2023-05-30 10:05:40 -0500

    Completed day 7 part 1.

--- 7/7.go
+++ 7/7.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
 	"os"
 	"strconv"
@@ -10,21 +9,22 @@ import (
 )
 
 type FileNode struct {
-	Name     string
-	IsDir    bool
-	FileSize int64
-	Children []*FileNode
+	Name 		string
+	Dir 		bool
+	Size 		int
+	Parent 		*FileNode
+	Children 	[]*FileNode
 }
 
 func PrintFileTree(node *FileNode, indent string) {
 	fmt.Print(indent + "-" + node.Name)
-	if node.IsDir {
-		fmt.Print(" (dir, ")
-		fmt.Print(node.FileSize)
+	if node.Dir {
+		fmt.Print(" (dir, size=")
+		fmt.Print(node.Size)
 		fmt.Print(")")
 	} else {
 		fmt.Print(" (file, size=")
-		fmt.Print(node.FileSize)
+		fmt.Print(node.Size)
 		fmt.Print(")")
 	}
 	fmt.Print("\n")
@@ -35,131 +35,103 @@ func PrintFileTree(node *FileNode, indent string) {
 }
 
 func main() {
+	// setup read file
+	file, err := os.Open("7input.txt")
+	if err != nil {
+		fmt.Println("Error opening file: ", err)
+		return
+	}
+	defer file.Close()
+
+	// setup scanner
+	scanner := bufio.NewScanner(file)
+
+	// setup root node
 	root := new(FileNode)
 	root.Name = "/"
-	root.IsDir = true
+	root.Dir = true
 
-	err := buildFileTree(root)
-	if err != nil {
-		fmt.Println("An error occured while building the file tree: ", err)
+	// track current dir
+	currentDir := root
+
+	// Scan through all the commands and build file tree
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if (strings.HasPrefix(line, "$")) { // this is a command
+			tokens := strings.Split(line, " ")
+
+			if (tokens[1] == "cd") { // we need to change dirs
+				if (tokens[2] == "/") { // change back to root
+					currentDir = root
+				} else if tokens[2] != ".." { // need to step into
+					for _, child := range currentDir.Children {
+						if child.Name == tokens[2] {
+							currentDir = child
+						}
+					}
+				} else { // need to step out of
+					currentDir = currentDir.Parent
+				}
+
+				if currentDir == nil {
+					fmt.Println("An error occured while changing dirs.")
+					return
+				}
+			}
+		} else { // not a command, need to add files/dirs to file tree
+			tokens := strings.Split(line, " ")
+			node := new(FileNode)
+			node.Name = tokens[1]
+			node.Parent = currentDir
+			node.Dir = tokens[0] == "dir"
+
+			if (!node.Dir) {
+				size, err := strconv.Atoi(tokens[0])
+				if err != nil {
+					fmt.Println("There was an error converting a file size from string to int.")
+					return
+				}
+				node.Size = size
+			}
+
+			currentDir.Children = append(currentDir.Children, node)
+		}
 	}
 
 	calcDirSizes(root)
-	listOfDirs := []int64 {}
-	listOfDirs = findDirs(root, listOfDirs)
-
-	for i, dir := range listOfDirs {
-		fmt.Println(i, ": ", dir)
-	}
-
-	var total int64
-	total = 0
-	for _, dir := range listOfDirs {
-		total += dir
-	}
-
-	PrintFileTree(root, "")
-	fmt.Println(total)
-
+	// PrintFileTree(root, "")
+	fmt.Println("Advent of Code Day 7 Part 1 Result: ", calculateTotalSize(root))
 }
 
-func buildFileTree(root *FileNode) error {
-	currentLocation := root
-	
-	file, err := os.Open("7input.txt")
-	if err != nil {
-		return errors.New("error opening file")
-	}
-	defer file.Close()
-	
-	scanner := bufio.NewScanner(file)
-	
-	for scanner.Scan() {
-		line := scanner.Text()
-		if strings.HasPrefix(line, "$") {
-			tokens := strings.Split(line, " ")
-			if tokens[1] == "ls" {
-				continue
-			} else {
-				currentLocation = handleCommand(tokens[2], root, currentLocation)
-			}
-		} else {
-			tokens := strings.Split(line, " ")
-			node := new(FileNode)
-			node.IsDir = tokens[0] == "dir"
-			if !node.IsDir {
-				size, err := strconv.Atoi(tokens[0])
-				if err != nil {
-					return errors.New("error converting file size string to int")
-				}
-				node.FileSize = int64(size)
-			}
-
-			node.Name = tokens[1]
-
-			currentLocation.Children = append(currentLocation.Children, node)
-		}
+func calcDirSizes(node *FileNode) int {
+	if !node.Dir {
+		return node.Size
 	}
 
-	return nil
-}
-
-func handleCommand(command string, root *FileNode, current *FileNode) *FileNode {
-	if command == ".." {
-		return findTarget(root, current.Name, true)
-	}
-
-	return findTarget(root, command, false)
-}
-
-func findTarget(node *FileNode, target string, stepOut bool) *FileNode {
-	if node.Name == target {
-		return node
-	}
-	
-	for _, child := range node.Children {
-		tempNode := findTarget(child, target, stepOut)
-		if tempNode != nil {
-			if (stepOut) {
-				return node
-			}
-			return tempNode
-		}
-	}
-
-	return nil
-}
-
-func calcDirSizes(node *FileNode) int64 {
-	if node.Name == "jqrdvnbp" {
-		fmt.Println("asdf")
-	}
-
-	if !node.IsDir {
-		return node.FileSize
-	}
-
-	var dirSize int64
-	dirSize = 0
+	dirTotal := 0
 
 	for _, child := range node.Children {
 		childSize := calcDirSizes(child)
-		dirSize += childSize
+		dirTotal += childSize
 	}
 
-	node.FileSize = dirSize
-	return dirSize
+	node.Size = dirTotal
+	return dirTotal
 }
 
+func calculateTotalSize(node *FileNode) int {
+	var totalSize int
 
-func findDirs(node *FileNode, dirs []int64) []int64 {
-	if node.IsDir && node.FileSize <= 100000 {
-		dirs = append(dirs, node.FileSize)
+	if node.Dir {
+		for _, child := range node.Children {
+			totalSize += calculateTotalSize(child)
+		}
+
+		if node.Size <= 100000 {
+			totalSize += node.Size
+		}
 	}
 
-	for _, child := range node.Children {
-		dirs = findDirs(child, dirs)
-	}
-
-	return dirs
+	return totalSize
 }
